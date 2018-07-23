8f07158c76e43a74f6add7587574ff880eda0a1a
Andrei Shamanau <iamlowhp@gmail.com>
2018-07-24 00:00:30 +0300

    Add support for counters.

--- src/background.js
+++ src/background.js
@@ -1,3 +1,24 @@
-chrome.runtime.onMessage.addListener(function(message) {
-    chrome.browserAction.setBadgeText({text: message.data});
+chrome.runtime.onStartup.addListener(function() {
+    chrome.browserAction.setBadgeBackgroundColor({color: "#474a4f"});
+});
+
+
+chrome.runtime.onMessage.addListener(function(message, sender) {
+    if (message.count) {
+        chrome.tabs.get(sender.tab.id, function(tab) {
+            if (tab.index >= 0) {
+                chrome.browserAction.setBadgeBackgroundColor({color: "#474a4f"});
+                chrome.browserAction.setBadgeText({tabId:tab.id, text:message.count > 0 ? message.count.toString() : ''});
+            } else {
+                var tabId = sender.tab.id, text = message.count > 0 ? message.count.toString() : '';
+                chrome.webNavigation.onCommitted.addListener(function update(details) {
+                    if (details.tabId === tabId) {
+                        chrome.browserAction.setBadgeBackgroundColor({color: "#474a4f"});
+                        chrome.browserAction.setBadgeText({tabId: tabId, text: text});
+                        chrome.webNavigation.onCommitted.removeListener(update);
+                    }
+                });
+            }
+        });
+    }
 });
--- src/inject.js
+++ src/inject.js
@@ -2,6 +2,14 @@
 
     var count = document.getElementsByTagName("canvas").length;
 
+    if (window.document.frames){
+        window.document.frames.forEach(function(frame) {
+            var frameDoc = frame.contentDocument || frame.contentWindow.document;
+            var frameCount = frameDoc.getElementsByTagName("canvas").length;
+            count = count + frameCount;
+        });
+    }
+
     chrome.runtime.sendMessage(chrome.runtime.id, {count: count});
 
 }());
