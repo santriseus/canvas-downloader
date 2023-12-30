680ce33cadbe38ba75a613b7b1b6b343d5082347
andrei.shamanau <andrei.shamanau@softeq.com>
2023-12-30 03:15:40 +0200

    Migrate to manifest V3

--- src/service_worker.js
+++ src/service_worker.js
@@ -1,13 +1,13 @@
-chrome.browserAction.setBadgeBackgroundColor({color: "#474a4f"});
+chrome.action.setBadgeBackgroundColor({color: "#474a4f"});
 
 chrome.runtime.onMessage.addListener(function(message, sender) {
     if (message.count && message.count > 0) {
         console.log("Starting");
-        chrome.browserAction.getBadgeText({tabId:sender.tab.id}, (text) => {
+        chrome.action.getBadgeText({tabId:sender.tab.id}, (text) => {
             let value = text ? parseInt(text) : 0;
             console.log("Current state before: " + value);
             value = value + message.count;
-            chrome.browserAction.setBadgeText({tabId:sender.tab.id, text:value.toString()});
+            chrome.action.setBadgeText({tabId:sender.tab.id, text:value.toString()});
             console.log("Current state after: " + value);
         });
     }
